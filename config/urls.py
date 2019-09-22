5d190b2de2b20c872a99826a26709e7775f56b07
SeungHyeonTak <conficker77@gmail.com>
2019-09-22 21:20:23 +0900

    Login 부분 구현

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from suser.views import index, RegisterView
+from suser.views import index, RegisterView, LoginView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
 ]
--- suser/views.py
+++ suser/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {
+        'email': request.session.get('user')  # LoginView에서 form_valid가 끝날경우 session을 넘겨준다.
+    })
 
 
 class RegisterView(FormView):
     template_name = 'register.html'
     form_class = RegisterForm
-    success_url = '/' # 어떤 주소로 이동 시킬때
+    success_url = '/'  # 어떤 주소로 이동 시킬때
+
+
+class LoginView(FormView):
+    template_name = 'login.html'
+    form_class = LoginForm
+    success_url = '/'
+
+    # 유효성 검사가 끝났을때, 모든 데이터가 정상적일때 (로그인 데이터가 정상적일때) 들어오는 함수
+    def form_valid(self, form):
+        self.request.session['user'] = form.email
+        return super().form_valid(form)
